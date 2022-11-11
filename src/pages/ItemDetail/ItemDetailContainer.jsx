8787c6a3a422e24cbba0747d3c79313621d02c26
chivi <paulofr016@gmail.com>
2022-11-11 12:51:27 -0300

    tp 2

--- src/pages/ItemDetail/ItemDetailContainer.jsx
+++ src/pages/ItemDetail/ItemDetailContainer.jsx
@@ -3,37 +3,37 @@ import { useParams } from "react-router-dom"
 import { gFetch } from '../../utils/gFetch'
 
 const ItemDetailContainer = () => {
-  const [products, setProducts] = useState([])
+  const [product, setProduct] = useState([])
   const [loading, setLoading] = useState(true)
   const {id} = useParams();
-  console.log();
+  console.log(product);
   useEffect (()=>{
     gFetch()
-        .then(resp =>  setProducts(resp.find(prod => prod.id)))    
+        .then(resp =>  setProduct(resp.find(prod => prod.id)))    
         .catch(err => console.log(err))
         .finally(()=>setLoading(false)) 
         }       
-    , [])
+    , [id])
   return (
     loading
     ?
     <h2>Cargando...</h2>            
     :
     <div>
-      { products.map( obj =>  
-                                            <div key={obj.id} className='card w-50 mt-2'>
+      
+                                            <div key={product.id} className='card w-50 mt-2'>
                                                 <div className='card-header'>
-                                                    {obj.name}
+                                                    {product.name}
                                                 </div>
                                                 <div className='card-body'>
                                                     <center>
-                                                        <img src={obj.foto} className="w-50" />
+                                                        <img src={product.foto} className="w-50" />
                                                     </center>
                                                 </div>
                                                 <div className='card-footer'>
-                                                    precio : {obj.price}
+                                                    precio : {product.price}
                                                 </div>
-                                            </div>)}
+                                            </div>
     </div>
   )
 }
--- src/utils/gFetch.js
+++ src/utils/gFetch.js
@@ -3,7 +3,8 @@ let obj = [
     {id: 2 , name: 'PRODUCTO 2', categoria: 'remeras', stock: '100',price: '1500',  foto:'https://cdn.palbincdn.com/users/31244/images/GORRA-BASICA-JUNIOR-CUSTOMIZASHOPBF10B-COLOR-ROSA-1611838353.jpg'},
     {id: 3 , name: 'PRODUCTO 3', categoria: 'accesorios', stock: '100',price: '1500',  foto:'https://cdn.palbincdn.com/users/31244/images/GORRA-BASICA-JUNIOR-CUSTOMIZASHOPBF10B-COLOR-ROSA-1611838353.jpg'},
     {id: 4 , name: 'PRODUCTO 4', categoria: 'gorras', stock: '100',price: '1500',  foto:'https://cdn.palbincdn.com/users/31244/images/GORRA-BASICA-JUNIOR-CUSTOMIZASHOPBF10B-COLOR-ROSA-1611838353.jpg'},
-    {id: 5 , name: 'PRODUCTO 5', categoria: 'remeras', stock: '100',price: '1500',  foto:'https://cdn.palbincdn.com/users/31244/images/GORRA-BASICA-JUNIOR-CUSTOMIZASHOPBF10B-COLOR-ROSA-1611838353.jpg'}
+    {id: 5 , name: 'PRODUCTO 5', categoria: 'remeras', stock: '100',price: '1500',  foto:'https://cdn.palbincdn.com/users/31244/images/GORRA-BASICA-JUNIOR-CUSTOMIZASHOPBF10B-COLOR-ROSA-1611838353.jpg'},
+    {id: 6 , name: 'PRODUCTO 6', categoria: 'remeras', stock: '100',price: '1500',  foto:'http://placekitten.com/1040/1040'}
 ]
 
 export const gFetch = (id) => {
